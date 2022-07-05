ca3764bb68b4b03572bdc6a895da73e1de4e9638
Narawit Kaewbun <narawit.kaewbun@gmail.com>
2022-07-05 14:05:19 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'refin-sec2-2';
+  categories: any;
+  filteredCategories: any;
+  categoryTitle: any;
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit() {
+    this.http.get("https://api.publicapis.org/categories").subscribe((data: any) => {
+      this.categories = data.categories
+      this.filteredCategories = data.categories
+    })
+  }
+
+  handleChange() {
+    if (this.categoryTitle) {
+      this.filteredCategories = this.categories.filter((cat: any) => {
+        return cat.includes(this.categoryTitle)
+      })
+    } else {
+      this.filteredCategories = this.categories;
+    }
+  }
 }
